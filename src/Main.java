6b9340282bfbe8e1383bb57cd1ccfdda2d226fb5
Sara <saracoppola996@gmail.com>
2023-07-12 12:10:32 +0200

    checked statements

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        int a = 15;
+        a += 5;
+        a -= 4;
+        System.out.println(a);
+        int b = a;
+        b += 1;
+        System.out.println(b);
 
+        boolean firstStatement = b % 2 != 0;
+        boolean secondStatement = (b * (b + 1)) % 3 == 0;
+
+        System.out.println(firstStatement);
+        System.out.println(secondStatement);
     }
+
 }
